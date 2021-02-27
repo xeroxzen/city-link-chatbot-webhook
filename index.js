24429ff812db8aca2ffdd094919bdaad3391e7c4
Google Jr <andilembele020@gmail.com>
2021-02-27 07:35:19 +0200

    changed post url to /booking

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get("/", (req, res) => {
   res.send("Yes the server is live dude, go to bed.");
 });
 
-app.post("/dialogflow-fulfillment", express.json(), (req, res) => {
+app.post("/booking", express.json(), (req, res) => {
   const agent = new dfff.WebhookClient({
     request: req,
     response: res,
